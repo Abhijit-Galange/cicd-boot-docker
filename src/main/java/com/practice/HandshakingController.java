2485f47b0a5d8d274ef708da6fc7951ea1e64e60
Abhijit-Galange <abhijit.galange@gmail.com>
2024-12-06 23:29:43 +0530

    project added

---
+++ src/main/java/com/practice/HandshakingController.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HandshakingController {
+
+	@GetMapping("/hello")
+	public String handShake() {
+
+		return "Hello Docker World!!!";
+	}
+
+}
