4fb93ea76c69d1532a1c97bc65aaa2910b5cba28
Abhijit-Galange <abhijit.galange@gmail.com>
2025-03-18 03:50:50 +0530

    Update HandshakingController.java

--- src/main/java/com/practice/HandshakingController.java
+++ src/main/java/com/practice/HandshakingController.java
@@ -9,7 +9,7 @@ public class HandshakingController {
 	@GetMapping("/hello")
 	public String handShake() {
 
-		return "This is from Jenkins CICD pipeline. I'm available at DockerHub, running inside ec2 container and kubernetes [EKS] pod's container";
+		return "This is Jenkins CICD pipeline project. I'm available at DockerHub, running inside ec2 container and kubernetes [EKS] pod's container";
 	}
 
 }
