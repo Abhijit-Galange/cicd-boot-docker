1bccbd9eccbe68b14b2a003dfe9e7b37c1608e36
Abhijit-Galange <abhijit.galange@gmail.com>
2025-06-27 19:10:55 +0530

    "hello" URI modified, "which" URI added

--- src/main/java/com/practice/HandshakingController.java
+++ src/main/java/com/practice/HandshakingController.java
@@ -9,7 +9,13 @@ public class HandshakingController {
 	@GetMapping("/hello")
 	public String handShake() {
 
-		return "This is Jenkins CICD pipeline project. Technologies used Java SE 11, Maven, Git, Git Hub, Docker, Docker Hub, Ansible, AWS EC2, EKS";
+		return "Hi!!! This is Jenkins CICD pipeline project.";
+	}
+	
+	@GetMapping("/which")
+	public String getTechnologyName() {
+
+		return "Technologies used Java SE 11, Spring Boot, Spring Cloud, Maven, Git, Git Hub, Docker, Docker Hub, Ansible, AWS EC2, EKS";
 	}
 
 }
