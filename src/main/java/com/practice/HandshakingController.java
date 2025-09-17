6198a75340060cac6d53734fb28869e117497b7c
Abhijit-Galange <abhijit.galange@gmail.com>
2025-09-17 15:56:46 +0530

    endpoint URI and on screen messages are updated

--- src/main/java/com/practice/HandshakingController.java
+++ src/main/java/com/practice/HandshakingController.java
@@ -6,16 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HandshakingController {
 
-	@GetMapping("/hello")
+	@GetMapping("/whoami")
 	public String handShake() {
 
-		return "Hi!!! This is Jenkins CICD pipeline demo project.";
+		return "This is Jenkins CICD pipeline demo project.";
 	}
 	
-	@GetMapping("/which")
+	@GetMapping("/tech")
 	public String getTechnologies() {
 
-		return "Technologies used are Java SE 11, Spring Boot, Spring Cloud, Maven, Git, Git Hub, Docker, Docker Hub, Ansible, Kubernetes, EKS [Amazon Elastic Kubernetes Service]";
+		return "Technologies used are Java SE 11, Spring [Boot, Cloud], Maven, Git, Docker, Ansible, Kubernetes, EKS [Amazon Elastic Kubernetes Service].";
 	}
 	
 	@GetMapping("/cloud")
