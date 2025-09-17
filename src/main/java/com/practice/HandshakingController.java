48535c18fbb8cef636f0765241b53bf4e4fed7cf
Abhijit-Galange <abhijit.galange@gmail.com>
2025-09-17 15:29:55 +0530

    on screen messages are updated

--- src/main/java/com/practice/HandshakingController.java
+++ src/main/java/com/practice/HandshakingController.java
@@ -9,13 +9,13 @@ public class HandshakingController {
 	@GetMapping("/hello")
 	public String handShake() {
 
-		return "Hi!!! This is Jenkins CICD pipeline project.";
+		return "Hi!!! This is Jenkins CICD pipeline demo project.";
 	}
 	
 	@GetMapping("/which")
 	public String getTechnologies() {
 
-		return "Technologies used Java SE 11, Spring Boot, Spring Cloud, Maven, Git, Git Hub, Docker, Docker Hub, Ansible, Kubernetes";
+		return "Technologies used are Java SE 11, Spring Boot, Spring Cloud, Maven, Git, Git Hub, Docker, Docker Hub, Ansible, Kubernetes, EKS [Amazon Elastic Kubernetes Service]";
 	}
 	
 	@GetMapping("/cloud")
