70af2225e2db2f76ee2b943082b8af706a2b38ad
Abhijit-Galange <abhijit.galange@gmail.com>
2025-09-21 17:05:16 +0530

    URI endpoints are updated

--- src/main/java/com/practice/HandshakingController.java
+++ src/main/java/com/practice/HandshakingController.java
@@ -6,22 +6,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HandshakingController {
 
-	@GetMapping("/whoami")
+	@GetMapping("/hello")
 	public String handShake() {
 
-		return "This is Jenkins CICD pipeline demo project.";
+		return "This is CICD pipeline demo project";
+	}
+	
+	@GetMapping("/tool")
+	public String getToolName() {
+
+		return "Jenkins.";
 	}
 	
 	@GetMapping("/tech")
 	public String getTechnologies() {
 
-		return "Technologies used are Java SE 11, Spring [Boot, Cloud], Maven, Git, Docker, Ansible, Kubernetes, EKS [Amazon Elastic Kubernetes Service].";
+		return "Java SE 11, Spring [Boot, Cloud], DevOps: Git, Maven, Docker, Kubernetes, Ansible.";
 	}
 	
 	@GetMapping("/cloud")
 	public String getCloudPlatformName() {
 
-		return "Amazon Web Services (AWS) - EC2, IAM, EKS";
+		return "Amazon Web Services (AWS) - EC2, IAM, EKS [Elastic Kubernetes Service]";
 	}
 
 }
